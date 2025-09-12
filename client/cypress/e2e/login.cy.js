2d8dda9fa4af39e4825121a809890ba890474263
S3bas77 <sebasnocach@gmail.com>
2025-09-11 21:06:42 -0400

    feat: cypress terminal implementation

---
+++ client/cypress/e2e/login.cy.js
@@ -0,0 +1,15 @@
+describe('Login de usuario', () => {
+  it('Debería iniciar sesión correctamente con credenciales válidas', () => {
+    cy.visit('/login');
+
+    cy.get('#login_email').type('estudiante@example.com');
+    cy.get('#login_password').type('Estudiante123!');
+
+    cy.get('button[type="submit"]').click();
+
+    cy.task('log', 'Redireccionando al dashboard...');
+
+    cy.url().should('include', '/');
+    cy.contains('LEARNING ISC', { timeout: 10000 }).should('be.visible');
+  });
+});
