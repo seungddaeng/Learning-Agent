a192ef8e02978f247fb67004e542b8917b14ae7e
pablo-Acha <pabloate901@gmail.com>
2025-09-12 18:31:28 -0400

    feat[004] Change imports in the academic_management.controllers.ts and academic_management.module.ts files

--- backend/src/modules/academic_management/academic_management.module.ts
+++ backend/src/modules/academic_management/academic_management.module.ts
@@ -26,7 +26,7 @@ import { CreateCourseUseCase } from './application/commands/create-course.usecas
 import { SoftDeleteSingleEnrollmentUseCase } from './application/commands/soft-delete-single-enrollment.useCase';
 import { SaveAttendanceGroupStudentUseCase } from './application/commands/save-attendance-group-student-usecase';
 import { GetCoursesByTeacherUseCase } from './application/queries/get-courses-by-teacher.usecase';
-import { GetStudentAbsencesByDate } from './application/queries/get-date-absences-by-student.usecase.ts';
+import { getDateAbsencesBystudentUseCase } from './application/queries/get-date-absences-by-student.usecase.ts';
 import { GetClassesByCourseUseCase } from './application/queries/get-classes-by-course.usecase';
 import { GetAbsencesByClass } from './application/queries/get-absences-by-class';
 import { GetCourseByIdUseCase } from './application/queries/get-course-by-id.usecase';
@@ -64,7 +64,7 @@ import { AttendancePrismaRepository } from './infrastructure/persistence/attenda
     SoftDeleteClassUseCase,
     SaveAttendanceGroupStudentUseCase,
     GetAbsencesByClass,
-    GetStudentAbsencesByDate,
+    getDateAbsencesBystudentUseCase,
   ],
 })
 export class AcademicManagementModule {}
